b691a9293cb1a8665153f2f70d06a5521fce865b
runan13 <65391973+runan13@users.noreply.github.com>
2020-07-12 20:45:28 +0900

    Add files via upload

---
+++ login/main.js
@@ -0,0 +1,9 @@
+let id = $("#id");
+let pw = $("#pw");
+let btn = $("#btn");
+
+$(btn).on("click", function () {
+  if ($(id).val() == "") {
+    $(id).next("label").addClass("warning");
+  } else if ($(pw).val() == "") $(pw).next("label").addClass("warning");
+});
